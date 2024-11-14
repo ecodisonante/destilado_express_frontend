c6d673eeea9f3d6803f390fbb89b11bcd89dd9bb
Francisco Valdes <ecodisonante@gmail.com>
2024-11-14 00:56:50 -0300

    cambio menu con usuario logeado

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,9 +1,9 @@
 import { Component, HostListener, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
-// import { UserService } from '../../services/user.service';
 import { CommonModule } from '@angular/common';
-// import Swal from 'sweetalert2';
+import { UserService } from '../../services/user.service';
+import Swal from 'sweetalert2';
 
 /**
  * @description
@@ -18,53 +18,30 @@ import { CommonModule } from '@angular/common';
 })
 export class NavbarComponent {
 
-  /**
-   * Indicador si hay un usuario registrado
-   */
   isAuthenticated: boolean = false;
-  /**
-   * Indicador si hay un usuario registrado con permisos de acmin
-   */
   isAdmin: boolean = false;
-  /**
-   * Clase inicial aplicada al logo de la barra de navegacion
-   */
   logoClass: string = 'bar-logo-off';
 
-  /**
-   * constructor
-   */
   constructor(
-    // private userService: UserService, 
+    private userService: UserService,
     private router: Router
   ) { }
 
-  /**
-   * ngOnInit
-   */
   ngOnInit(): void {
-    // this.userService.isAuthenticated.subscribe((authStatus: boolean) => { this.isAuthenticated = authStatus; });
-    // this.userService.isAdminAuth.subscribe((adminStatus: boolean) => { this.isAdmin = adminStatus; });
+    this.userService.isAuthenticated.subscribe((authStatus: boolean) => { this.isAuthenticated = authStatus; });
+    this.userService.isAdminAuth.subscribe((adminStatus: boolean) => { this.isAdmin = adminStatus; });
   }
 
-  /**
-   * @description
-   * Termina la sesión del usuario activo.
-   */
   salir() {
-    // this.userService.logOut();
-    // Swal.fire({
-    //   icon: "success",
-    //   title: "Vuelve pronto !!",
-    // }).then(() => {
-    //   this.router.navigate(['/']);
-    // });
+    this.userService.logOut();
+    Swal.fire({
+      icon: "success",
+      title: "Vuelve pronto !!",
+    }).then(() => {
+      this.router.navigate(['/']);
+    });
   }
 
-  /**
-   * @description
-   * Vuelve visible el logo de la barra de navegación cuando llega a la parte superior de la pantalla
-   */
   @HostListener('window:scroll', [])
   onWindowScroll() {
     if (window.scrollY > 130) {
